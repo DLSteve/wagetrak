627fa84e69aec3fcf141b90beaf2bdede6ce0053
Steve <darksle@gmail.com>
2020-04-12 09:10:57 -0400

    added navebar and inital branding

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
+import SiteNav from './components/SiteNav'
 import EarningsTimer from "./components/EarningsTimer";
 import './App.scss';
 
 function App() {
   return (
       <div className="App">
+        <SiteNav/>
         <EarningsTimer/>
       </div>
   );
---
+++ src/components/SiteNav.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {Navbar, Button} from "rbx";
+
+export default function SiteNav() {
+  return (
+      <Navbar color="primary">
+        <Navbar.Brand>
+          <Navbar.Item href="/">
+            <img
+                src={process.env.PUBLIC_URL + '/images/wt_nav_logo.svg'}
+                alt=""
+                role="presentation"
+                width="174"
+                height="35"
+            />
+          </Navbar.Item>
+          <Navbar.Burger />
+        </Navbar.Brand>
+        <Navbar.Menu>
+          <Navbar.Segment align="end">
+            <Navbar.Item>
+              <Button.Group>
+                <Button color="link">
+                  <strong>Sign up</strong>
+                </Button>
+                <Button color="light">Log in</Button>
+              </Button.Group>
+            </Navbar.Item>
+          </Navbar.Segment>
+        </Navbar.Menu>
+      </Navbar>
+  )
+}
