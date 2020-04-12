93eec3741f7c93232ac46d81657b0bc2c485ff0a
Steve <darksle@gmail.com>
2020-04-12 10:11:48 -0400

    strict mode and minor modal refactor

--- src/components/OptionsModalManager.js
+++ src/components/OptionsModalManager.js
@@ -1,42 +1,45 @@
-import React, {useState, useCallback} from "react";
+import React, {useState} from "react";
 import {Modal, Button, Delete} from "rbx";
 
 export default function OptionsModalManager({onOpen, onClose, onSave, children}) {
   const [active, setActive] = useState(false);
-  const handleOpen = useCallback(() => {
+
+  const handleOpen = () => {
     onOpen()
     setActive(true)
-  }, [onOpen])
+  }
 
-  const handleClose = useCallback(() => {
+  const handleClose = () => {
     onClose()
     setActive(false)
-  }, [onClose])
+  }
 
-  const handleSave = useCallback(() => {
+  const handleSave = () => {
     onSave()
     setActive(false)
-  }, [onSave])
+  }
 
   return (
-      <div>
-        <Modal active={active} onClose={handleClose}>
-          <Modal.Background />
-          <Modal.Card>
-            <Modal.Card.Head>
-              <Modal.Card.Title>Options</Modal.Card.Title>
-              <Delete onClick={handleClose} />
-            </Modal.Card.Head>
-            <Modal.Card.Body>
-              {children}
-            </Modal.Card.Body>
-            <Modal.Card.Foot>
-              <Button color="success" onClick={handleSave}>Save changes</Button>
-              <Button onClick={handleClose}>Cancel</Button>
-            </Modal.Card.Foot>
-          </Modal.Card>
-        </Modal>
+      <>
         <Button color="primary" onClick={handleOpen}>Options</Button>
-      </div>
+        {active && (
+            <Modal active onClose={handleClose}>
+              <Modal.Background/>
+              <Modal.Card>
+                <Modal.Card.Head>
+                  <Modal.Card.Title>Options</Modal.Card.Title>
+                  <Delete onClick={handleClose}/>
+                </Modal.Card.Head>
+                <Modal.Card.Body>
+                  {children}
+                </Modal.Card.Body>
+                <Modal.Card.Foot>
+                  <Button color="success" onClick={handleSave}>Save changes</Button>
+                  <Button onClick={handleClose}>Cancel</Button>
+                </Modal.Card.Foot>
+              </Modal.Card>
+            </Modal>
+        )}
+      </>
   )
 }
